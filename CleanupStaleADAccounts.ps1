6322281db18ab09e4c937dd45c6a901eec3f7e54
Fabio Freire <fabio@fabio.nyc>
2018-03-25 20:47:58 -0400

    Documented properly. Works for computer accounts. Not implemented for user accounts

--- CleanupStaleADAccounts.ps1
+++ CleanupStaleADAccounts.ps1
@@ -3,63 +3,80 @@
 Disables and/or deletes stale computer and user accounts
 
 .DESCRIPTION
+Disables and/or deletes stale computer and user accounts
 - Supports exception list
 - Adds "Disabled on ". Writes to Location property but that can be changed easily
 - Send out status email
 
-Look over the default values below and change them to something that makes sense in your environment, otherwise
-I guarantee this will not work out of the box!
+Look over the default values below and change them to something that makes sense in your environment, otherwise I guarantee this will not work out of the box!
 
-WARNING: this script makes changes to AD objects which may be hard to reverse. Use the WhatIf parameter when in doubt
+WARNING: this script makes changes to AD objects which may be hard (or impossible) to reverse. Use the WhatIf parameter when in doubt
+
+WARNING: Setting deleteAfter < disableAfter could disable then immediately delete an object, or outright delete it!
 
 DISCLAIMER: use at your own risk!!
+
+.PARAMETER disableAfter
+Number of days *since last logon* after which a user/computer will be disabled. Set to 0 (zero) to skip
+
+.PARAMETER deleteAfter
+Number of days *since last logon* after which a user/computer will be deleted. Set to 0 (zero) to skip
+
+.PARAMETER exceptionListPath
+Path to file that contains exception list (one item per line. Users or computers)
+
+.PARAMETER logFilePath
+Path of log file (if any). Not specifying this will not create log
+
+.PARAMETER from
+From email address
+
+.PARAMETER to
+To email address
+
+.PARAMETER smtpServer
+SMTP server address. Not specifying this will not send email
+
+.PARAMETER disabledComputersOU
+OU where disabled computer accounts will be moved to
+
+.PARAMETER disabledUsersOU
+OU where disabled user accounts will be moved to
+
+.PARAMETER disabledOnTextADPropertyName
+AD Property for "Disabled On ..." text
+
+.PARAMETER processComputers
+Process computer accounts? Default is $True
+
+.PARAMETER processUsers
+Process user accounts? Default is $True
+
+.PARAMETER WhatIf. Default it $True. Specify $False to do actual work
+Dry-run
 
 .EXAMPLE
 .\CleanStaleADObjects.ps1 -disableAfter 90 -deleteAfter 180 -whatIf
 #>
 
 Param(
-    [Parameter(Mandatory=$False,HelpMessage="Number of days before object is considered stale. Set to 0 to skip")]
-    [int]$disableAfter,
-
-    [Parameter(Mandatory=$False,HelpMessage="Number of days before object is deleted. Set to 0 to skip")]
-    [int]$deleteAfter,
-
-    [Parameter(Mandatory=$False,HelpMessage="Exception list (one item per line. Users or computers")]
-    [string[]]$exceptionListPath = ".\staleObjectsExceptionList.txt",
-
-    [Parameter(Mandatory=$False,HelpMessage="Path of log file (if any)")]
+    [int]$disableAfter = 0,
+    [int]$deleteAfter = 30,
+    [string]$exceptionListPath = ".\staleObjectsExceptionList.txt",
     [string]$logFilePath = "d:\logs\staleObjects.log",
-
-    [Parameter(Mandatory=$False,HelpMessage="From email address")]
     [string]$from = "staleObjects@domain.com",
-
-    [Parameter(Mandatory=$False,HelpMessage="To email address")]
     [string]$to = "peopleWhoCareAboutStaleObjects@domain.com",
-
-    [Parameter(Mandatory=$False,HelpMessage="SMTP server address")]
-    [string]$smtpServer = "smtp.domain.com",
-
-    [Parameter(Mandatory=$False,HelpMessage="OU to move disabled computers to")]
-    [string]$disabledComputersOU = "OU=disabledComputers,DC=domain,DC=com",
-
-    [Parameter(Mandatory=$False,HelpMessage="OU to move disabled users to")]
-    [string]$disabledUsersOU = "OU=xEmployees,DC=domain,DC=com",
-
-    [Parameter(Mandatory=$False,HelpMessage="AD Property for Disabled On text")]
+    [string]$smtpServer,
+    [string]$disabledComputersOU = "OU=disabledComputers,$($(Get-ADDomain).DistinguishedName)",
+    [string]$disabledUsersOU = "OU=xEmployees,$($(Get-ADDomain).DistinguishedName)",
     [string]$disabledOnTextADPropertyName = "Location",
-
-    [Parameter(Mandatory=$False,HelpMessage="Process computer accounts?")]
     [boolean]$processComputers = $true,
-
-    [Parameter(Mandatory=$False,HelpMessage="Process user accounts?")]
     [boolean]$processUsers = $true,
-
-    [Parameter(Mandatory=$False,HelpMessage="Set this to see what the script *would* do")]
     [switch]$whatIf = $true
 )
 
-$Days           = (Get-Date).Adddays(-$disableAfter)
+$disableDays    = (Get-Date).Adddays(-$disableAfter)
+$deleteDays     = (Get-Date).Adddays(-$deleteAfter)
 $Today          = (Get-Date -UFormat "%Y-%m-%d")
 $disabledOnText = @{$disabledOnTextADPropertyName = ("Disabled On $Today") }
 
@@ -68,29 +85,38 @@ if (Test-Path $exceptionListPath) {
 }
 
 # Process Computers
-if ($processComputers -and $disableAfter -ge 0) {
-    $Computers = Get-ADComputer -Filter {(LastLogonDate -lt $Days) -and (Enabled -eq $true)} |`
-         ? { ($exceptionList -notcontains $_.Name) } | sort -Property Name
+if ($processComputers) {
+    if ($disableAfter -gt 0) {
+        $Computers = Get-ADComputer -Filter {(LastLogonDate -lt $disableDays) -and (Enabled -eq $true)} |`
+             ? { ($exceptionList -notcontains $_.Name) } | sort -Property Name
 
-    # Disable account(s)
-    if ($whatIf) {
-        $Computers | Set-ADComputer $disabledOnText -WhatIf
-        $Computers | Disable-ADAccount -WhatIf
-        $Computers | Move-ADObject -TargetPath $disabledComputersOU -WhatIf
-    } else {
-        $Computers | Set-ADComputer $disabledOnText
-        $Computers | Disable-ADAccount
-        $Computers | Move-ADObject -TargetPath $disabledComputersOU
+        # Disable computer account(s)
+        if ($whatIf) {
+            $Computers | Set-ADComputer @disabledOnText -WhatIf
+            $Computers | Disable-ADAccount -WhatIf
+            $Computers | Move-ADObject -TargetPath $disabledComputersOU -WhatIf
+        } else {
+            $Computers | Set-ADComputer @disabledOnText
+            $Computers | Disable-ADAccount
+            $Computers | Move-ADObject -TargetPath $disabledComputersOU
+        }
     }
-    #Delete accounts that were moved to Disabled after 6 months
-    $Computers = Get-ADComputer –Filter * -SearchBase $disabledComputersOU | Where-Object {$_.Location -GT (Get-Date).AddMonths(-7) -and $_.Location -LT (Get-Date).AddMonths(-6)}
-    $Computers | Remove-ADObject
+    if ($deleteAfter -gt 0) {
+        $Computers = Get-ADComputer -SearchBase $disabledComputersOU -Filter {(LastLogonDate -lt $deleteDays) -and (Enabled -eq $false)}
+
+        # Delete computer account(s)
+        if ($whatIf) {
+            $Computers | Remove-ADObject -WhatIf
+        } else {
+            $Computers | Remove-ADObject
+        }
+    }
 }
 
 
 # Process User
 if ($processUsers) {
-    $Users = Get-ADUser -Filter {(LastLogonDate -lt $Days) -and (Enabled -eq $true)} -Properties Name, LastLogonDate, PasswordLastSet, PasswordNeverExpires |`
+    $Users = Get-ADUser -Filter {(LastLogonDate -lt $disableDays) -and (Enabled -eq $true)} |`
         ? { ($exceptionList -notcontains $_.Name) } | sort -Property Name
 
     #$Users | Select-Object -Property Name, Enabled, LastLogonDate, PasswordLastSet, PasswordNeverExpires | sort -Property Name  | Export-Csv c:\temp\staleUsers.csv
@@ -108,13 +134,14 @@ if ($processUsers) {
 }
 
 #Prepare email
-$messageParameters = @{
-    From = "staleObjects@sandleroneill.com"
-    #To = "cis-it-tech@sandleroneill.com"
-    To = "ffreire@sandleroneill.com"
-    SmtpServer = "sop-exch2010.sop.com"
-    Subject = "Disabled AD Objects"
-    Body = ($computers | Select-Object -Property Name, DNSHostName, Enabled, LastLogonDate | out-string -Width 99)
-}
+if ($smtpServer) {
+    $messageParameters = @{
+        From = $from
+        To = $to
+        SmtpServer = $smtpServer
+        Subject = "Disabled AD Objects"
+        Body = ($computers | Select-Object -Property Name, DNSHostName, Enabled, LastLogonDate | out-string -Width 99)
+    }
 
-Send-MailMessage @messageParameters
+    Send-MailMessage @messageParameters
+}
